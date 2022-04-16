c2b09fb134b526773199538ba642f0e81211f7fa
Shraddha <shraddhamanjramkar2@gmail.com>
2022-04-16 22:56:01 +0530

    Calculator using Switch

---
+++ Java/calculator.java
@@ -0,0 +1,39 @@
+import java.util.*;
+class calculator
+{
+    public static void main(String args[])
+    {
+        int a,b;
+        int result;
+        int op;
+        System.out.println("Enter two numbers");
+        Scanner s=new Scanner(System.in);
+        a=s.nextInt();
+        b=s.nextInt();
+        System.out.println("Enter Your Choice: \n 1 for Addition \n 2 for Substraction \n 3 for Multiplication \n 4 for Division");
+        op=s.nextInt();
+    
+     switch(op)
+     {
+        case 1 :
+           result=a+b;
+           System.out.println("Sum is "+ result);
+           break;
+        case 2 :
+           result=a-b;
+           System.out.println("Sub is "+ result);
+           break;
+        case 3 :
+           result=a*b;
+           System.out.println("Mul is "+ result);
+           break;
+        case 4 :
+           result=a/b;
+           System.out.println("Div is "+ result);
+           break;
+        default:
+           System.out.println("Wrong input");
+           break;
+     }
+    }
+}
