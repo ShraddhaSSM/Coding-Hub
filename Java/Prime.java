a74960f0ab6a27fa2e8420f697b2b5e458c220bc
Shraddha <shraddhamanjramkar2@gmail.com>
2022-04-22 22:42:57 +0530

    Prime number between two internals

---
+++ Java/Prime.java
@@ -0,0 +1,28 @@
+import java.util.*;
+public class Prime {
+
+    public static void main(String args[])
+     {
+
+        int low = 50, high = 100;
+
+        while (low < high)
+         {
+            boolean flag = false;
+
+            for(int i = 2; i <= low/2; ++i) 
+            {
+                if(low % i == 0) 
+                {
+                    flag = true;
+                    break;
+                }
+            }
+
+            if (!flag && low != 0 && low != 1)
+                System.out.print(low + " ");
+
+            ++low;
+            }
+    }
+}
