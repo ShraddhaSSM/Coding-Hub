19e4067a996d2e5fb246aff475beec5ebad6caa8
Shraddha <shraddhamanjramkar2@gmail.com>
2022-04-24 00:17:30 +0530

    Reverse Number

---
+++ Java/reverseNum.java
@@ -0,0 +1,20 @@
+import java.util.*;
+class reverseNum
+{
+  public static void main(String args[])
+   {
+    int num, reversed = 0;
+    System.out.println("Enter number: ");
+    Scanner s=new Scanner(System.in);
+    num=s.nextInt();
+    System.out.println("Original Number: " + num);
+    while(num != 0) 
+    {
+      int digit = num % 10;
+      reversed = reversed * 10 + digit;
+      num /= 10;
+    }
+
+    System.out.println("Reversed Number: " + reversed);
+    }
+}
