238c0ecd4f82ba101271c8e51ae981e5f2a8a73c
Shraddha <shraddhamanjramkar2@gmail.com>
2022-04-26 19:50:38 +0530

    Finding Remainder

---
+++ Java/remainder.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class remainder
+ {
+
+  public static void main(String args[])
+   {
+
+    int dividend = 99, divisor = 2;
+
+    int quotient = dividend / divisor;
+    int remainder = dividend % divisor;
+
+    System.out.println("Quotient = " + quotient);
+    System.out.println("Remainder = " + remainder);
+  }
+}
