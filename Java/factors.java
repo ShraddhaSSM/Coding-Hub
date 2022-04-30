4f60b4a4a988d4ce5075b62b3951d7e6e6d19798
Shraddha <shraddhamanjramkar2@gmail.com>
2022-04-30 21:01:26 +0530

    Finding Factorials

---
+++ Java/factors.java
@@ -0,0 +1,20 @@
+import java.util.*;
+class factors
+{
+
+  public static void main(String[] args)
+   {
+    int number;
+    System.out.print("Enter number : ");
+    Scanner s=new Scanner(System.in);
+    number=s.nextInt();
+    System.out.print("Factors of " + number + " are: ");
+    for (int i = 1; i <= number; ++i)
+    {
+      if (number % i == 0) 
+      {
+        System.out.print(i + " ");
+      }
+    }
+   }
+}
