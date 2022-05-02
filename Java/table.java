1fc578efbaacb8eb94e54d19d2da1136c60f3af4
Shraddha <shraddhamanjramkar2@gmail.com>
2022-05-02 22:43:01 +0530

    Multiplication Table

---
+++ Java/table.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class table 
+{
+
+    public static void main(String[] args)
+    {
+        int num;
+        System.out.println("Enter number of which table you want : ");
+        Scanner s=new Scanner(System.in);
+        num=s.nextInt();
+        for(int i = 1; i <= 10; ++i)
+        {
+            System.out.printf("%d * %d = %d \n", num, i, num * i);
+        }
+    }
+}
