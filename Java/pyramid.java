9704d97b48687371f196ab818ccf8d96c372a925
Shraddha <shraddhamanjramkar2@gmail.com>
2022-05-06 23:32:38 +0530

    Pyramid Pattern

---
+++ Java/pyramid.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class pyramid 
+{    
+public static void main(String args[])   
+{    
+int i, j, row = 6;        
+for (i=0; i<row; i++)   
+{     
+for (j=row-i; j>1; j--)   
+{
+System.out.print(" ");   
+}   
+for (j=0; j<=i; j++ )   
+{   
+System.out.print("* ");   
+}  
+System.out.println();   
+}   
+}   
+}  
