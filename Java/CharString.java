3ff6183fef505d8c71ad3857dff81d6ac19b1ff7
Shraddha <shraddhamanjramkar2@gmail.com>
2022-05-07 22:08:22 +0530

    Character to String

---
+++ Java/CharString.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class CharString 
+{
+
+    public static void main(String args[]) 
+    {
+        char ch = 'a';
+        String st = Character.toString(ch);
+        System.out.println("The string is: " + st);
+    }
+}
