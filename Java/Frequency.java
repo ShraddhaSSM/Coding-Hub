3fd34189d49439af3590fef0ea407e245b79d007
Shraddha <shraddhamanjramkar2@gmail.com>
2022-05-08 23:18:52 +0530

    Frequency of a Character

---
+++ Java/Frequency.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class Frequency
+ {
+
+    public static void main(String args[])
+    {
+        String str = "I am Shraddha";
+        char ch = 'a';
+        int frequency = 0;
+
+        for(int i = 0; i < str.length(); i++)
+        {
+            if(ch == str.charAt(i)) 
+            {
+                ++frequency;
+            }
+        }
+
+        System.out.println("Frequency of " + ch + " = " + frequency);
+    }
+}
