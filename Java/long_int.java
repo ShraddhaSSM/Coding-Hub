b82ad3c53eaa94b44e14a63aa41fcedda29a6401
Shraddha <shraddhamanjramkar2@gmail.com>
2022-05-10 23:12:18 +0530

    Long to Int

---
+++ Java/long_int.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class long_int
+{
+  public static void main(String[] args) 
+  {
+    long a = 2322331L;
+    long b = 52341241L;
+    int c = (int)a;
+    int d = (int)b;
+
+    System.out.println(c);   
+    System.out.println(d);    
+  }
+}
