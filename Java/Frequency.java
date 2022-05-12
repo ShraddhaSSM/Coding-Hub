3d8652e156e46e10c544b18e156777392a0f0600
Shraddha <shraddhamanjramkar2@gmail.com>
2022-05-12 22:07:57 +0530

    Changes

--- Java/Frequency.java
+++ Java/Frequency.java
@@ -5,7 +5,7 @@ public class Frequency
     public static void main(String args[])
     {
         String str = "I am Shraddha";
-        char ch = 'a';
+        char ch = 'd';
         int frequency = 0;
 
         for(int i = 0; i < str.length(); i++)
