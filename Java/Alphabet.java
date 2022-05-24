3a3a5e92927286878fb134d7bca0580d3245ac3a
Shraddha <shraddhamanjramkar2@gmail.com>
2022-05-24 21:40:21 +0530

    Changes

--- Java/Alphabet.java
+++ Java/Alphabet.java
@@ -4,6 +4,7 @@ class Alphabet
   public static void main(String[] args)
    {
     char c;
+    System.out.println("All Alphabets are : ");
     for(c = 'A'; c <= 'Z'; ++c)
       System.out.print(c + " ");
     }
