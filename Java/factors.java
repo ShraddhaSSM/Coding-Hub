843c67a08954e7c660dd6b5a17317206b50649fb
Shraddha <shraddhamanjramkar2@gmail.com>
2022-05-25 22:31:17 +0530

    Changes

--- Java/factors.java
+++ Java/factors.java
@@ -5,10 +5,10 @@ class factors
   public static void main(String[] args)
    {
     int number;
-    System.out.print("Enter number : ");
+    System.out.print("Enter the number : ");
     Scanner s=new Scanner(System.in);
     number=s.nextInt();
-    System.out.print("Factors of " + number + " are: ");
+    System.out.print("Factors of the given " + number + " are: ");
     for (int i = 1; i <= number; ++i)
     {
       if (number % i == 0) 
