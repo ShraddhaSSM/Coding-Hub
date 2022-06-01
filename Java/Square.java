22dd9f7ae288daa46fbb3a5404854e1fce7a8952
Shraddha <shraddhamanjramkar2@gmail.com>
2022-06-02 00:03:03 +0530

    Printing Square

---
+++ Java/Square.java
@@ -0,0 +1,15 @@
+public class Square 
+{
+  public static void main(String[] args) 
+  {
+    int size = 5;
+    for (int i = 0; i < size; i++) 
+    {
+      for (int j = 0; j < size; j++) 
+      {
+        System.out.print("*");
+      }
+      System.out.println();
+    }
+  }
+}
