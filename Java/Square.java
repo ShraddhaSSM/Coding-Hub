d6d604a70fba81ed621b2f62eccc18166d9b485b
Shraddha <shraddhamanjramkar2@gmail.com>
2022-06-03 23:54:54 +0530

    Changes

--- Java/Square.java
+++ Java/Square.java
@@ -3,6 +3,7 @@ public class Square
   public static void main(String[] args) 
   {
     int size = 5;
+    System.out.println("Pattern is");
     for (int i = 0; i < size; i++) 
     {
       for (int j = 0; j < size; j++) 
