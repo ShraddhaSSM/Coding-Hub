d1953e9dd1a62c58ff46446ec6bd668ef336dbd1
Shraddha <shraddhamanjramkar2@gmail.com>
2022-06-05 00:09:20 +0530

    Changes

--- Java/pyramid.java
+++ Java/pyramid.java
@@ -3,7 +3,8 @@ public class pyramid
 {    
 public static void main(String args[])   
 {    
-int i, j, row = 6;        
+int i, j, row = 6;  
+System.out.println("..Pyramid..");    
 for (i=0; i<row; i++)   
 {     
 for (j=row-i; j>1; j--)   
